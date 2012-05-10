2aabf7bd5c2117a3acff8fd683b491500f86d004
Emil Ronnback <emilronnback@gmail.com>
2012-05-10 15:05:14 +0800

    test commit 1

---
+++ try1/Try1.cpp
@@ -0,0 +1,19 @@
+#include "try1.h"
+#include <iostream>
+int main(int argc, char** argv) 
+{
+
+	std::cerr << "test print" << std::endl;
+	getchar(); // have to press [return] to exit.
+	return 0;
+}
+Try1::Try1(void)
+{
+
+}
+
+
+Try1::~Try1(void)
+{
+
+}
---
+++ try1/Try1.h
@@ -0,0 +1,9 @@
+#pragma once
+int main(int argc, char** argv);
+class Try1
+{
+public:
+	Try1(void);
+	~Try1(void);
+};
+
